059a143688afe411a48f1c48b55882a83ee5a5cb
starmun-0010 <abdemuqsit@hotmail.com>
2023-10-08 21:37:39 +0500

    Add window hints

--- src/main.zig
+++ src/main.zig
@@ -3,21 +3,42 @@ const c = @cImport({
     @cInclude("glad/glad.h");
     @cInclude("GLFW/glfw3.h");
 });
-pub fn main() void {
-    std.debug.print("Learni\n", .{});
-    const success = c.glfwInit();
-    if (success == 0) {
-        std.debug.panic("Failed to init GLFW", .{});
-    }
-    var window = c.glfwCreateWindow(1920, 1080, "Learn OpenGL", null, null);
 
+pub fn main() void {
+    const SCR_WIDTH: u32 = 1920;
+    const SCR_HEIGHT: u32 = 1080;
+
+    const glfw_init_result = c.glfwInit();
+    if (glfw_init_result == 0) {
+        std.debug.panic("Failed to initialize GLFW", .{});
+    }
+    defer c.glfwTerminate();
+
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+    c.glfwWindowHint(
+        c.GLFW_OPENGL_PROFILE,
+        c.GLFW_OPENGL_CORE_PROFILE,
+    );
+    c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+
+    const window = c.glfwCreateWindow(
+        SCR_WIDTH,
+        SCR_HEIGHT,
+        "Learn OpenGL",
+        null,
+        null,
+    );
     if (window == null) {
         std.debug.panic("Failed to create GLFW window", .{});
     }
-
     c.glfwMakeContextCurrent(window);
 
-    if (c.gladLoadGLLoader(@as(c.GLADloadproc, @ptrCast(&c.glfwGetProcAddress))) == 0) {
-        std.debug.panic("Failed to initialise GLAD\n", .{});
+    const glad_load_result = c.gladLoadGLLoader(@as(
+        c.GLADloadproc,
+        @ptrCast(&c.glfwGetProcAddress),
+    ));
+    if (glad_load_result == 0) {
+        std.debug.panic("Failed to load GLAD.", .{});
     }
 }
