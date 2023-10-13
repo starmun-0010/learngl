6d5c58bfa5760d378a3210e53c255ecea3c6d6e5
starmun-0010 <abdemuqsit@hotmail.com>
2023-10-13 23:49:45 +0500

    color from vertex shader

--- src/main.zig
+++ src/main.zig
@@ -9,17 +9,20 @@ const SCR_HEIGHT: u32 = 1080;
 const vertex_shader_source: [:0]const u8 =
     \\#version 330 core
     \\layout (location = 0) in vec3 aPos;
+    \\out vec4 vertexColor;
     \\void main()
     \\{
     \\  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\  vertexColor = vec4(0.5f, 0.5f, 0.2f, 1.0f);
     \\};
 ;
 const fragment_shader_source: [:0]const u8 =
     \\#version 330 core
+    \\in vec4 vertexColor;
     \\out vec4 FragColor;
     \\void main()
     \\{
-    \\  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    \\  FragColor = vertexColor;
     \\}
 ;
 pub fn main() void {
